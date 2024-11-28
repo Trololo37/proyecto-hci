220dc92e3a892ec69355796be7d0af81d37d7f6a
trol <a348814@uach.mx>
2024-11-27 21:43:26 -0600

    Modificado para poder hacer varias solicitudes durante una sola ejecución del programa

--- voiceDetector.py
+++ voiceDetector.py
@@ -4,10 +4,10 @@ import serial
 import time
 import os
 
-# Configuración del puerto serial
-ser = serial.Serial('COM4', baudrate=115200, timeout=10)   # Cambia COM4 por el puerto correcto
-time.sleep(2)  # Espera a que el puerto serial esté listo
+ser = serial.Serial('COM4', baudrate=115200, timeout=10)
+time.sleep(2.5)  
 r = sr.Recognizer() 
+time.sleep(0.5)
 
     #
     #esptool --chip esp32s3 --port COM4 erase_flash
@@ -15,13 +15,10 @@ r = sr.Recognizer()
     #
 
 def send_command(command):
-    """
-    Envía un comando al ESP32 por el puerto serial y espera una respuesta.
-    """
-    ser.write((command + '\n').encode('utf-8'))  # Envía el comando al ESP32
+    ser.write((command + '\n').encode('utf-8')) 
     print(f"Command sent: {command}")
-    time.sleep(1)  # Espera un momento para que el ESP32 procese el comando
-    response = ser.read_all().decode('utf-8').strip()  # Lee la respuesta del ESP32
+    time.sleep(0.1)
+    response = ser.read_all().decode('utf-8').strip()
     if response:
         print(f"Response from ESP32: {response}")
     else:
@@ -32,35 +29,40 @@ def nombre_a_numero(palabra):
         "uno": '1',
         "dos": '2',
         "tres": '3',
-        "cuatro": '4'
+        "cuatro": '4',
     }
-    print(nombres_a_numeros.get(palabra.lower(), "Número no reconocido"))
     return nombres_a_numeros.get(palabra.lower(), "Número no reconocido")
 
 
 while True:
     try:
         with sr.Microphone() as source:
-            print('Hola, soy tu asistente por voz: ')
+            print('\nHola, soy tu asistente por voz: ')
             print("Elige entre decir uno, dos, tres o cuatro")
             print("para prender los distintos focos, o di salir")
-            time.sleep(0.5)
-            print("\ndilo ahora")
+            print("Ahora, espera la señal :)")
+            time.sleep(1.5)
+            print("\nDilo ahora")
             audio = r.listen(source)
-            #time.sleep(0.5)
             try:
                 print("traduciendo")
                 text = r.recognize_google(audio, language="es")
                 print('Has dicho: {}'.format(text))
-                print(text)
-
                 user_input = str(text).strip()
-                print(user_input)
-
                 texto = nombre_a_numero(user_input)
+                time.sleep(0.5)
 
                 if texto in ['1', '2', '3', '4']:
-                    send_command(texto)  # Enviar el comando al ESP32
+                    send_command(texto)
+                    time.sleep(0.5)
+                elif "uno" in text or '1' in text:
+                    send_command(nombre_a_numero("uno"))
+                elif "dos" in text or '2' in text:
+                    send_command(nombre_a_numero("dos"))
+                elif "tres" in text or '3' in text:
+                    send_command(nombre_a_numero("tres"))
+                elif "cuatro" in text or '4' in text:
+                    send_command(nombre_a_numero("cuatro"))
                 elif text.strip()=="salir":
                     ser.close()
                     break
@@ -71,12 +73,12 @@ while True:
            #         print("Confirmar cambios")
             #    if "cancelar" in text:
              #       print("Cancelar cambios")
-            except:
+            except Exception as e:
                 print('No te he entendido')
+                print(e)
             time.sleep(0.5)
-            os.system('cls' if os.name=='nt' else 'clear')
+            #os.system('cls' if os.name=='nt' else 'clear')
     except KeyboardInterrupt:
         print("Cancelacion con teclado")
-        break
-    finally:
-        ser.close()  # Cierra el puerto serial al terminar
+        ser.close() 
+        break        
