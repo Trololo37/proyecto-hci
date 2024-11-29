5be204cf61a095c7720033a39e90aaf715498b47
Adora <im.stariluz@gmail.com>
2024-11-28 19:51:55 -0600

    Working on other devices with ip network

--- webpage/server.js
+++ webpage/server.js
@@ -12,6 +12,6 @@ app.use(express.static('public'));
 
 
 // Inicia el servidor
-app.listen(port, () => {
-    console.log(`Servidor funcionando en http://localhost:${port}`);
+app.listen(port, "192.168.100.11",() => {
+    console.log(`Servidor funcionando en http://${"192.168.100.11"}:${port}`);
 });
